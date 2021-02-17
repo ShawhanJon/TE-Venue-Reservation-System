24e4bc533590b2123fea5c86e52c8502388bb3a6
Charlie Nguyen <cnguyen1286@gmail.comm>
2021-02-16 19:02:16 -0500

    part 1 completed

--- 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.ProjectDAO;
 
@@ -20,17 +22,59 @@ public class JDBCProjectDAO implements ProjectDAO {
 	
 	@Override
 	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
+		
+	List<Project> allProjects = new ArrayList<Project>();
+		
+		String sql = "SELECT project.*" +
+					 "FROM project"; 
+					 
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		
+		while(results.next()) {
+		Project project = mapRowToProject(results);
+		allProjects.add(project);
+			}
+		
+		return allProjects;
+		
 	}
+	
+		
+		
 
 	@Override
 	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
 		
-	}
+jdbcTemplate.update("DELETE FROM project_employee WHERE project_id = ? AND employee_id = ?" ,projectId, employeeId);
+}
+		
+
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
 		
+		String employeeToProjectSQL = "INSERT INTO project_employee (project_id, employee_id )" +
+											"VALUES(?, ?)";
+		
+		jdbcTemplate.update(employeeToProjectSQL, 
+				                   
+				                     projectId,
+				                     employeeId
+				                     
+				                     );
+		
+		
 	}
+	
+	private Project mapRowToProject(SqlRowSet results) {
+		Project project = new Project();
+		
+		project.setId(results.getLong("project_id"));
+		project.setName(results.getString("name"));
+		project.setEndDate(results.getDate("from_date").toLocalDate());
+		project.setEndDate(results.getDate("to_date").toLocalDate());
+		
+		return project;
+	}	
 
 }
